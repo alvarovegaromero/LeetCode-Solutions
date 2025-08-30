5c6603680f30d1fd793bd76f728af18c8882d16d
Álvaro <vegaromeroalvaro@gmail.com>
2025-08-30 17:31:34 +0200

    feat: add alternative approach for "jump game" problem with goal shrinking method

--- solutions/Array-String/55. Jump Game.java
+++ solutions/Array-String/55. Jump Game.java
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/jump-game/
 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class Solution {
     public boolean canJump(int[] nums) {
         int maxPosReachable = 0;
@@ -12,5 +14,22 @@ class Solution {
         }
 
        return true;
+    }
+}
+
+// Other approach using goal shrinking
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
     }
 }
