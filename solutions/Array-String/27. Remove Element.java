3354dd8b52e07e42c4d4a5b79164c7e78d286c00
Álvaro <vegaromeroalvaro@gmail.com>
2025-10-12 13:14:37 +0200

    feat: "remove element" problem solved

--- solutions/Array-String/27. Remove Element.java
+++ solutions/Array-String/27. Remove Element.java
@@ -1,5 +1,40 @@
 //https://leetcode.com/problems/remove-element/
 
+// First approachs
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int lastValidIndex = nums.length - 1;
+        int k = 0;
+
+        for (int i = 0; i <= lastValidIndex; i++) {
+            if (nums[i] == val) {
+                // Find a value different than val   
+                while (lastValidIndex > i && nums[lastValidIndex] == val) {
+                    lastValidIndex--;
+                }
+
+                // If valid element found, swap
+                if (lastValidIndex > i) {
+                    int tmp = nums[i];
+                    nums[i] = nums[lastValidIndex];
+                    nums[lastValidIndex] = tmp;
+                    lastValidIndex--;
+                }
+            }
+
+            if (nums[i] != val) {
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
+// Two pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class Solution {
     public int removeElement(int[] nums, int val) {
         int indexValidNums = 0;
